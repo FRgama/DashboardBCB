759a757e8778c1c6986cdde60289f32072f95a4a
Rodrigo Gama <121040163+FRgama@users.noreply.github.com>
2025-05-17 01:27:39 -0300

    ajuste nos nomes da pagina de créditos

--- src/pages/Créditos.py
+++ src/pages/Créditos.py
@@ -6,11 +6,11 @@ st.markdown("""
 
 
 
-### Integrantes:
-            #Beatriz de Souza Santos Rio Branco
-            #Sátiro Gabriel de Souza Santos 
-            #Sabrinna Cristina Gomes Vicente
-            #Rodrigo Correa da Gama
+### Integrantes:-
+- **Beatriz de Souza Santos Rio Branco**  
+- **Sátiro Gabriel de Souza Santos**  
+- **Sabrinna Cristina Gomes Vicente**  
+- **Rodrigo Correa da Gama**  
 ---
 
 Este painel foi desenvolvido como parte de um trabalho acadêmico para explorar indicadores econômicos usando Python e Streamlit.
